00c94d6ed502546b8ca79869b70aae9b7432229e
Abdul Kadir <ak98.narutokun@gmail.com>
2019-03-16 20:35:59 +0530

    Add POST/user with firebase

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -1,4 +1,6 @@
 
+const firebaseAdmin = require('../modules/firebase').FirebaseApp;
+const db = firebaseAdmin.firestore();
 
 exports.user_scores = (req, res) => {
 
@@ -7,8 +9,39 @@ exports.user_scores = (req, res) => {
 
 exports.add_user = (req, res) => {
 
-    res.send('NOT IMPLEMENTED');
+    // var docRef = db.collection('users').doc('alovelace');
 
+    // docRef.set({
+    //   first: 'Ada',
+    //   last: 'Lovelace',
+    //   born: 1815
+    // }).then(() => {
+    //         console.log("written to database!");
+
+    //         res.send({success: "true"});
+    // });
+
+    const user = {
+        address: req.body.address,
+        dob: req.body.dob,
+        domains: req.body.domains,
+        email: req.body.email,
+        geekID: req.body.geekID,
+        geekScore: req.body.geekScore,
+        gender: req.body.gender,
+        linkedin: req.body.linkedin,
+        name: req.body.name,
+        newsletter: req.body.newsletter,
+        portfolio: req.body.portfolio,
+        projects:req.body.projects
+    }
+
+    db.collection('users').doc().set(user).then(() => {
+        console.log("Saved user to database");
+        res.send({success: "true"});
+    });
+
+      
 }
 
 exports.update_user_score = (req, res) => {
